c9d369f6f60e47db13ae6aafd5f321ecb7c9dcce
Jason Priem <jp@jasonpriem.org>
2019-04-28 09:50:02 -0700

    table works.

--- src/views/Journals.vue
+++ src/views/Journals.vue
@@ -4,29 +4,34 @@
 
         <v-layout>
             <v-card>
-                <v-list two-line>
-                    <template v-for="(result, index) in results">
-                        <v-list-tile :key="result.title">
-                            <v-list-tile-content>
-                                <v-list-tile-title v-html="result.journal_name"></v-list-tile-title>
-
-                            </v-list-tile-content>
 
 
+                <v-data-table
+                  :headers="headers"
+                  :items="results"
+                  class="elevation-1"
+                >
+                  <template v-slot:items="props">
+                    <td >{{ props.item.journal_name }}</td>
+                    <td class="text-xs-right">{{ props.item.num_dois }}</td>
+                    <td class="text-xs-right">{{ parseInt(props.item.proportion_oa * 100) }}</td>
+                    <td class="text-xs-right">{{ parseInt(props.item.proportion_bronze * 100)}}</td>
+                    <td class="text-xs-right">{{ parseInt(props.item.proportion_green * 100)}}</td>
+                    <td class="text-xs-right">{{ parseInt(props.item.proportion_hybrid * 100)}}</td>
+                  </template>
+                </v-data-table>
 
 
-                        </v-list-tile>
 
-
-
-
-                    </template>
-
-
-
-                </v-list>
-
-
+<!--                <v-list two-line>-->
+<!--                    <template v-for="(result, index) in results">-->
+<!--                        <v-list-tile :key="result.title">-->
+<!--                            <v-list-tile-content>-->
+<!--                                <v-list-tile-title v-html="result.journal_name"></v-list-tile-title>-->
+<!--                            </v-list-tile-content>-->
+<!--                        </v-list-tile>-->
+<!--                    </template>-->
+<!--                </v-list>-->
 
             </v-card>
 
@@ -47,7 +52,25 @@
     export default {
         name: 'Journals',
         data: () => ({
-           results: []
+           results: [],
+            headers: [
+                {text: "Journal name", value: "journal_name"},
+                {text: "Num DOIs", value: "num_dois"},
+                {text: "Any OA (%)", value: "proportion_oa"},
+                {text: "Bronze (%)", value: "proportion_bronze"},
+                {text: "Green (%)", value: "proportion_green"},
+                {text: "Hybrid (%)", value: "proportion_hybrid"}
+            ],
+
+
+            headers1: [
+                "Journal name",
+                "Num DOIs",
+                "Total OA (%)",
+                "Bronze (%)",
+                "Green (%)",
+                "Hybrid (%)"
+            ]
         }),
         methods: {
             fetch(){
@@ -76,6 +99,18 @@
 
 <style scoped lang="scss">
 
+    table.v-table tbody {
+        td {
+            font-size: 16px;
+            &:first-child {
+                padding-top: 10px;
+                padding-bottom: 10px;
+
+            }
+        }
+
+
+    }
 
 
 </style>
